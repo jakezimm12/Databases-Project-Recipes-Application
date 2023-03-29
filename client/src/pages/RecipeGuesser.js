fe595a952c9e60a55ad1f3a655d600d7d3d29c29
jakezimm12 <jakezimm12@gmail.com>
2023-03-29 19:53:41 -0400

    Starter code based on HW2 code

---
+++ client/src/pages/RecipeGuesser.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+import { Container, Divider, Link } from '@mui/material';
+import { NavLink } from 'react-router-dom';
+
+// import LazyTable from '../components/LazyTable';
+// import SongCard from '../components/SongCard';
+const config = require('../config.json');
+
+export default function RecipeGuesser() {
+    const [testValues, setTest] = useState([]);
+
+    useEffect(() => {
+      fetch(`http://${config.server_host}:${config.server_port}/recipe_guesser`)
+        .then(res => res.json())
+        .then(resJson => setTest(resJson));
+    }, []);
+  
+
+    return (
+        <Container>
+            Hello from inside RecipeGuesser component.\n
+            Values of getting the testtable inside our project's database: id: {testValues.id}, name: {testValues.name}
+        </Container>
+    );
+};
